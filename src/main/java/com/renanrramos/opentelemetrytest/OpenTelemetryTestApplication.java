9f2f97d59fbc3f772a3d73b321ca1b37e1d1f06f
Renan Ramos <renan.ramos@inatel.br>
2020-08-27 09:23:57 -0300

    First commit

---
+++ src/main/java/com/renanrramos/opentelemetrytest/OpenTelemetryTestApplication.java
@@ -0,0 +1,13 @@
+package com.renanrramos.opentelemetrytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTelemetryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTelemetryTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanrramos/opentelemetrytest/OpenTelemetryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanrramos.opentelemetrytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTelemetryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
