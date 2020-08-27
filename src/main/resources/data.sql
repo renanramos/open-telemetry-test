64cad1e683f0604479089a53c5b861a0e78c7632
Renan Ramos <renan.ramos@inatel.br>
2020-08-27 10:06:14 -0300

    Incluido db H2 e definidos schemas e data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRODUCTS (name, description) VALUES
+  ("Item 1", "This is the first item"),
+  ("Item 2", "This is the second item"),
+  ("Item 3", "This is the third item"),
+  ("Item 4", "This is the fourth item"),
+  ("Item 5", "This is the fifth item");
